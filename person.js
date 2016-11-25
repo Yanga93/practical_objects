b3841ece8832d1f273ac3bd99611eed7f62ec67a
Yanga93 <yangazukelwa@gmail.com>
2016-11-25 15:31:18 +0200

    Object with many keys that point to specific values

---
+++ person.js
@@ -0,0 +1,12 @@
+var person = {
+  firstName : "Jo",
+  lastName : 'Dlamini',
+  age : 27,
+  employed : true,
+  qualification : ['matric', 'nat cert in plumbing'],
+};
+//dot notation
+console.log(person.lastName);
+
+//index notation
+console.log(person['age']);
